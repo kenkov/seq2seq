f0ca855751a90c27f2409dab6734c90791206271
Noriyuki Abe <kenko.py@gmail.com>
2016-02-15 17:44:43 +0900

    Implement encoder_decoder

--- seq2seq.py
+++ seq2seq.py
@@ -204,7 +204,12 @@ def train_decoder(
                 pair_words = conversation[index]
 
                 # encoder input words
-                input_words_with_s = tokens2ids(pair_words[0], dictionary)
+                orig_words = pair_words[0][:-1]  # remove END_SYMBOL
+                reply_words = pair_words[1]
+                if orig_words:
+                    assert orig_words[-1] is not config.END_SYMBOL
+                input_words_with_s = tokens2ids(orig_words, dictionary)
+
                 ys, state = encoder_model.predictor.forward(
                     [Variable(
                         decoder_model.xp.array(
@@ -218,7 +223,9 @@ def train_decoder(
                 )
 
                 # decode
-                output_words_with_s = tokens2ids(pair_words[1], dictionary)
+                assert reply_words[0] == config.END_SYMBOL
+                assert reply_words[-1] == config.END_SYMBOL
+                output_words_with_s = tokens2ids(reply_words, dictionary)
                 try:
                     new_loss = decoder_model(
                         output_words_with_s,
@@ -319,6 +326,168 @@ def train_decoder(
         )
 
 
+def train_encoder_decoder(
+    model,
+    dictionary: corpora.Dictionary,
+    conversation_file: str,
+    decoder_model_dir: str,
+    epoch_size: int=100,
+    batch_size: int=30,
+    dropout: bool=False,
+    gpu: bool=False
+) -> None:
+    if gpu >= 0:
+        model.to_gpu()
+        print(model.xp)
+
+    # setup SGD optimizer
+    opt = optimizers.SGD()
+    opt.setup(model)
+
+    # optimizer hooks
+    clip_threshold = 5.0
+    print("set optimizer clip threshold: {}".format(clip_threshold))
+    opt.add_hook(chainer.optimizer.GradientClipping(clip_threshold))
+
+    # load conversation sentences
+    conversation = load_conversation(conversation_file, dictionary)
+    data_size = len(conversation)
+
+    print("data size: {}".format(data_size))
+    for epoch in range(epoch_size):
+        print("running epoch {}".format(epoch))
+        indexes = np.random.permutation(range(data_size))
+        epoch_loss = 0  # int
+
+        for bat_i in range(0, data_size, batch_size):
+            forward_start_time = datetime.now()
+            batch_loss = Variable(model.xp.zeros((), dtype=np.float32))
+
+            for index in indexes[bat_i:bat_i + batch_size]:
+                pair_words = conversation[index]
+
+                # encoder input words
+                orig_words = pair_words[0][:-1]  # remove END_SYMBOL
+                reply_words = pair_words[1]
+                if orig_words:
+                    assert orig_words[-1] is not config.END_SYMBOL
+                input_words_with_s = tokens2ids(orig_words, dictionary)
+                ys, state = model.predictor.forward(
+                    [Variable(
+                        model.xp.array(
+                            [word],
+                            dtype=model.xp.int32
+                        )
+                    ) for word in input_words_with_s],
+                    state=None,
+                    dropout=dropout,
+                    train=True
+                )
+
+                # decode
+                assert reply_words[0] == config.END_SYMBOL
+                assert reply_words[-1] == config.END_SYMBOL
+                output_words_with_s = tokens2ids(reply_words, dictionary)
+                try:
+                    new_loss = model(
+                        output_words_with_s,
+                        state=state,  # init_state を input の state にする
+                        dropout=dropout,
+                        train=True
+                    )
+                    batch_loss += new_loss
+                except Exception:
+                    print(index, input_words_with_s)
+                    import traceback
+                    traceback.print_exc()
+            # 平均化
+            batch_size_array = model.xp.array(
+                batch_size,
+                dtype=model.xp.float32
+            )
+            batch_loss = batch_loss / Variable(batch_size_array)
+            epoch_loss += batch_loss.data
+
+            # 時間計測
+            forward_end_time = datetime.now()
+
+            # 最適化
+            opt_start_time = datetime.now()
+            model.zerograds()
+            batch_loss.backward()
+            opt.update()
+            opt_end_time = datetime.now()
+
+            forward_delta = forward_end_time - forward_start_time
+            opt_delta = opt_end_time - opt_start_time
+            # print(
+            #     ("decoder epoch {} batch {}: loss {}, "
+            #      "forward {}, optimizer {},").format(
+            #         epoch,
+            #         int(bat_i / batch_size),
+            #         batch_loss.data,
+            #         forward_delta,
+            #         opt_delta,
+            #     )
+            # )
+            print_fmt = (
+                "epoch {} batch {}: "
+                "loss {}, grad L2 norm: {}, forward {}, optimizer {}"
+            )
+            print(print_fmt.format(
+                epoch,
+                int(bat_i / batch_size),
+                batch_loss.data,
+                opt.compute_grads_norm(),
+                forward_delta,
+                opt_delta,
+            ))
+            # save
+            if ((bat_i / batch_size) + 1) % 100 == 0:
+                serializers.save_npz(
+                    os.path.join(
+                        decoder_model_dir,
+                        "model.npz"
+                    ),
+                    model
+                )
+            if ((bat_i / batch_size) + 1) % 1000 == 0:
+                serializers.save_npz(
+                    os.path.join(
+                        decoder_model_dir,
+                        "model_{}_{}_{}.npz".format(
+                            epoch,
+                            int(bat_i / batch_size) + 1,
+                            datetime.now().strftime("%Y%m%d-%H%M%S")
+                        )
+                    ),
+                    model
+                )
+        print("finish epoch {}, loss {}".format(
+            epoch,
+            epoch_loss / epoch_size
+        ))
+        # save
+        serializers.save_npz(
+            os.path.join(
+                decoder_model_dir,
+                "model.npz"
+            ),
+            model
+        )
+        serializers.save_npz(
+            os.path.join(
+                decoder_model_dir,
+                "model_{}_{}_{}.npz".format(
+                    epoch,
+                    int(bat_i / batch_size) + 1,
+                    datetime.now().strftime("%Y%m%d-%H%M%S")
+                )
+            ),
+            model
+        )
+
+
 def decode(
         words: List[str],
         encoder_model,
@@ -326,6 +495,8 @@ def decode(
         dictionary: corpora.Dictionary,
         dropout: bool=False,
 ) -> List[str]:
+
+    assert words[-1] is not config.END_SYMBOL
 
     input_words_with_s = tokens2ids(words, dictionary, verbose=True)
     # input words の hidden state を求める
